b3260ba89e51813b7907a94b6e7b2439001570f0
Mangalio <119160852+Mangalio@users.noreply.github.com>
2024-05-05 11:31:57 +0300

    Add files via upload

---
+++ mark/gamemario/mario/script.js
@@ -0,0 +1,32 @@
+
+document.getElementById('animatedbutton1').addEventListener('mouseenter', function() {
+    this.style.transform = 'scale(1.1)';
+  });
+  
+  document.getElementById('animatedbutton1').addEventListener('mouseleave', function() {
+    this.style.transform = 'scale(1)';
+  });
+
+  document.getElementById('animatedbutto21').addEventListener('mouseenter', function() {
+    this.style.transform = 'scale(1.1)';
+  });
+  
+  document.getElementById('animatedbutton2').addEventListener('mouseleave', function() {
+    this.style.transform = 'scale(1)';
+  });
+
+  document.getElementById('animatedbutton3').addEventListener('mouseenter', function() {
+    this.style.transform = 'scale(1.1)';
+  });
+  
+  document.getElementById('animatedbutton3').addEventListener('mouseleave', function() {
+    this.style.transform = 'scale(1)';
+  });
+
+  document.getElementById('animatedbutton4').addEventListener('mouseenter', function() {
+    this.style.transform = 'scale(1.1)';
+  });
+  
+  document.getElementById('animatedbutton4').addEventListener('mouseleave', function() {
+    this.style.transform = 'scale(1)';
+  });
